874121db5c500afdd1d05565d6af2279cdd010d6
Yanawut Pomkaew <AnathamPK@gmail.com>
2022-06-30 11:26:56 +0700

    permutation in cpp

--- permutation.cpp
+++ permutation.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 using namespace std;
 
+void swaped(char *x, char *y) {
+
+    char temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void permuted(char *a, int k, int n) {
+
+   
+    if(k==n) {
+        cout << a << "\n" ;
+
+    }else {
+        for (int i = k; i<=n; ++i) {
+            swaped((a+k), (a+i));
+            permuted(a, k+1, n);
+            swaped((a+k), (a+i));
+        }
+    }
+}
+
 int main() {
 
-    int a;
+    char a[] = "ABCD";
+    
+    int size = (sizeof(a)/sizeof(a[0]))-2;
+    
+    permuted(a, 0, size);
 
     return 0;
 }
